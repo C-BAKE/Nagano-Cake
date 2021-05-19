59c7162051d0aa36d52c8d97d8b231414e27555c
magi954 <utawareikka587@gmail.com>
2021-05-19 12:11:18 +0000

    default: false migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_070607) do
+ActiveRecord::Schema.define(version: 2021_05_19_120749) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_070607) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
