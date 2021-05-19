53e22019722a131e6615a25cfdbb4f1d94be7794
eri237 <coconatsu3104@gmail.com>
2021-05-19 06:14:00 +0000

    topページ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -10,12 +10,4 @@ class Admin::SessionsController < ActionController::Base
   def destroy
   end
 
-  def after_sign_in_path_for(resource)
-    admin_top_path
-  end
-
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
-
 end
