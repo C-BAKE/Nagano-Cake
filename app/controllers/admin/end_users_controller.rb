a210d2d5059f06b55ef61c5544d3a12a4592d995
eri237 <coconatsu3104@gmail.com>
2021-05-19 04:36:52 +0000

    second commit

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,2 +1,14 @@
 class Admin::EndUsersController < ApplicationController
+  def index
+    @end_users = End_user.page(params[:page])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,2 +1,21 @@
-class Admin::SessionsController < ApplicationController
+class Admin::SessionsController < ActionController::Base
+
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def after_sign_in_path_for(resource)
+    admin_top_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
 end
