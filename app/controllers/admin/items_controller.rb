14f0c90a199f07856b24656e8c4b631a1cb8eacd
ytmk0116 <ytmk0116@gmail.com>
2021-05-20 05:14:30 +0000

    items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,25 @@
 class Admin::ItemsController < ApplicationController
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
+
+
   def create
+    @item = Item.new(item_params)
+  	if @item.save
+  	   redirect_to admin_item_path(@item)
+  	else
+  	   render "new"
+  	end
   end
 
   def show
+
   end
 
   def edit
@@ -16,4 +27,12 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+
+  def item_params
+  	params.require(:item).permit(:genre_id,:item_name,:unit_price_without_tax,:sale_status,:explanation,:image)
+  end
+
+
 end
