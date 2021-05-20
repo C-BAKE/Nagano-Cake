9b070e9355993b2ccf469d7dd6493269b9585909
magi954 <utawareikka587@gmail.com>
2021-05-20 02:25:38 +0000

    エラーあり

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,8 @@
 class Admin::ItemsController < ApplicationController
+
   def index
   end
-
+  
   def new
   end
 
@@ -12,8 +13,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+  	if @item.update(item_params)
+  	  redirect_to admin_item_path(@item.id)
+  	else
+  		render "edit"
+  	end
   end
+
+  private
+
+  def item_params
+  	params.require(:item).permit(:genre_id,:item_name,:unit_price_without_tax,:sale_status,:explanation,:image)
+  end
+  
 end
