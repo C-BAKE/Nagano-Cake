3428096522d261584598f1c65450b89bc0e54a22
magi954 <utawareikka587@gmail.com>
2021-05-25 05:53:08 +0000

    ggg

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,8 +5,8 @@ class Admin::OrderDetailsController < ApplicationController
 		@ordered_item = OrderedItem.find(params[:id]) #　特定
 		@order = @ordered_item.order #注文商品から注文テーブルの呼び出し（何度も呼び出すのは処理が増える為）
 		@ordered_item.update(ordered_item_params) #　製作ステータスの更新
-		# @order_item = Order.OderItem.find(params[:id])
-		# @order_item.update(order_item_params)
+		# @ordered_item = Order.OderedItem.find(params[:id])
+		# @ordered_item.update(ordered_item_params)
 
 		if @ordered_item.production_status == "製作中" #製作ステータスが「製作作中」なら入る
 			@order.update(order_status: 2) #注文ステータスを「製作中」　に更新
@@ -22,8 +22,10 @@ class Admin::OrderDetailsController < ApplicationController
 
 	private
 
-	  def ordered_item_params
-      params.require(:ordered_item).permit(:production_status)
+
+	def ordered_item_params
+     params.require(:ordered_item).permit(:production_status)
     end
+
 end
-  
+
